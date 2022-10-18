d3deb2ffeb10c9c2cd3bc96d245c7e521f016cfa
AhmadMajali <amajali392@gmail.com>
2022-10-18 18:05:56 +0600

    new adds

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 // Аль Маджали Ахмад, второй месяц андроида, группа с 4 по 6, домашнее задание 4 урока
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-     ArrayList<String> A = new ArrayList<>();
-     A.add("A1");
-     A.add("A2");
-     A.add("A3");
-     A.add("A4");
-     A.add("A5");
-        for (String name:A
-             ) {
-            System.out.println(name);
+        ArrayList<String> A = new ArrayList<>();
+        A.add("A1");
+        A.add("A2");
+        A.add("A3");
+        A.add("A4");
+        A.add("A5");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(A.get(i));
         }
+
         ArrayList<String> B = new ArrayList<>();
         B.add("B1");
         B.add("B2");
         B.add("B3");
         B.add("B4");
         B.add("B5");
+
+        Collections.reverse(B);
+
         ArrayList<String> C = new ArrayList<>();
         Integer x = 0;
         while (C.size() < A.size() + B.size()) {
@@ -29,8 +32,8 @@ public class Main {
             C.add(B.get(x));
             x++;
         }
-        C.add(A.get(0));
-        C.add(B.get(1));
+
+
         System.out.println(C);
         Collections.sort(C, new Comparator<String>() {
             @Override
